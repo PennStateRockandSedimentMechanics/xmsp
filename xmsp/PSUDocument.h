be8c09f3b32bee774c3aa78fe3f413511553ad6c
Ryan Martell <rdm4@martellventures.com>
2012-12-04 12:47:27 -0500

    Initial Commit

---
+++ xmsp/PSUDocument.h
@@ -0,0 +1,13 @@
+//
+//  PSUDocument.h
+//  xmsp
+//
+//  Created by Ryan Martell on 12/4/12.
+//  Copyright (c) 2012 Martell Ventures, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PSUDocument : NSDocument
+
+@end
---
+++ xmsp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  xmsp
+//
+//  Created by Ryan Martell on 12/4/12.
+//  Copyright (c) 2012 Martell Ventures, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
